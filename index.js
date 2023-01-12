f70264c68f7d1235f9b88259496ff180b9eaba1f
George Elkik <gelkik@gmail.com>
2023-01-12 10:02:11 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = record => {
+    const win = record.find(c =>c.result === 'W')
+    if (win != undefined){
+        return win['year'];
+    }
+    else{
+        return undefined;
+    }
+}
